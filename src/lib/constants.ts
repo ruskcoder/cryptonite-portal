0f1b1f20c71f59940b1102062607f70f006b3561
ruskcoder <118460956+ruskcoder@users.noreply.github.com>
2025-12-23 16:32:35 -0600

    fix: updated script url and events

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,5 @@
 // Google Apps Script API for event checking
-export const GOOGLE_SCRIPT_API = 'https://script.google.com/macros/s/AKfycbx_amL9dgOsRLuZtvhcrp1HavEkyPYKL11y1rUHPhR4A87LSvse19PPogbcLLpWgEqN/exec'
-
-// Allowed event titles for clock in/out
-export const ALLOWED_EVENT_TITLES = ['RSC Activities', 'RSC Activities']
+export const GOOGLE_SCRIPT_API = 'https://script.google.com/macros/s/AKfycbwTWLm2EiH1lzbJZbO0rtzRq2Iw2tBB0tP1F4RodMKnd0QMi3Vs9Fu_RGM2jb5idPFC/exec'
 
 // Allowed locations for clock in/out
 // Address, latitude, longitude, and radius in meters
@@ -11,7 +8,7 @@ export const ALLOWED_LOCATIONS = [
     address: '1730 Katyland Dr, Katy, TX 77493',
     lat: 29.796067947796566,
     lng: -95.81033897344692,
-    radius: 100000, 
+    radius: 500, 
   },
 ]
 
